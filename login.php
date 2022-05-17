d4b44c2ed4b23320e08fe5f9013935af5b5b3b98
olidiaks <oliwierstepniak@gmail.com>
2022-05-17 09:56:38 +0200

    Add form for add skis.
    # Conflicts:
    #	login.php

--- login.php
+++ login.php
@@ -1,17 +1,15 @@
 <?php
 
-if (
-    (isset($_POST['e-mail']) && isset($_POST['password'])) ||
-    (isset($_COOKIE['email']) && isset($_COOKIE['password']))
-) {
-    $email = $_POST['e-mail'] ?? $_COOKIE['email'];
-    $password = $_POST['password'] ?? $_COOKIE['password'];
+if (isset($_POST['e-mail']) && isset($_POST['password'])) {
+    $email = $_POST['e-mail'];
+    $password = $_POST['password'];
     include 'database.php';
-    $sql = "select * from Users where email = '$email' and password = '$password';";
+    echo $sql = "select * from Users where email = '$email' and password = '$password';";
     /** @noinspection PhpUndefinedVariableInspection */
     $query = $database->query($sql) or die('Nie udał się wyciągnąć z bazy danych id użytkownika.<br>' . $database->error);
+    $num_rows = $query->num_rows or die('Nie udało się policzyć ilu jest użytkowników o podanych danych<br>' . $database->error);
     $database->close() or die('Nie udało się poprawnie zamknąć połączenia z bazą danych<br>' . $database->error);
-    if ($query->num_rows == 1) {
+    if ($num_rows == 1) {
         session_start();
         $_SESSION['id'] = $query->fetch_array()['id'] or die('Nie udał się przypisać id użytkownika do sesji.<br>');
         $_SESSION['role'] = $query->fetch_array()['role'] or die('Nie udał się przypisać id użytkownika do sesji.<br>');
@@ -19,6 +17,7 @@ if (
             setcookie("email", $email, strtotime('+ 1 year'));
             setcookie("password", $password, strtotime('+ 1 year', $secure = true));
         }
+        header("Location: main.html");
     } else {
         include 'login.html';
         echo '<p>E-mail lub hasło są nie poprawne.</p>';
