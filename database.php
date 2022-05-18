b774b860c49ecef8bf5d47acdb24f90f2f136105
olidiaks <oliwierstepniak@gmail.com>
2022-05-18 10:30:45 +0200

    Add table Skis.

--- database.php
+++ database.php
@@ -7,6 +7,18 @@ $database = new mysqli(
     "wypozyczalnia-nart"
 ) or die('Nie udało się połączyć z bazą danych. <br>' . $database->error);
 
-$sql = "CREATE TABLE IF NOT EXISTS `Users` ( `id` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(65) NOT NULL , `password` VARCHAR(65) NOT NULL , `role` VARCHAR(14) NOT NULL ,PRIMARY KEY (`id`))  ENGINE = InnoDB;";
+$sql = "CREATE TABLE IF NOT EXISTS `Users` (
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    `email` VARCHAR(65) NOT NULL ,
+    `password` VARCHAR(65) NOT NULL ,
+    `role` VARCHAR(14) NOT NULL ,
+    PRIMARY KEY (`id`))  ENGINE = InnoDB;";
+$database->query($sql) or die('Nie udało się utworzyć nowych baz danych.' . $database->error);
 
+$sql = "CREATE TABLE  IF NOT EXISTS `Skis` (
+        `id` INT NOT NULL AUTO_INCREMENT ,
+        `brand` VARCHAR(65) NOT NULL , 
+        `model` VARCHAR(65) NOT NULL , 
+        `length` SMALLINT NOT NULL , 
+        PRIMARY KEY (`id`)) ENGINE = InnoDB;";
 $database->query($sql) or die('Nie udało się utworzyć nowych baz danych.' . $database->error);
