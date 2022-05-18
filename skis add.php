ca3bab5cd83d9463d980f50824b649b535f880e4
olidiaks <oliwierstepniak@gmail.com>
2022-05-18 13:17:26 +0200

    Add skis adding module.

--- skis add.php
+++ skis add.php
@@ -1,1 +1,15 @@
 <?php
+session_start();
+$id = $_SESSION["id"];
+$role = $_SESSION["role"];
+$brand = $_GET["brand"];
+$model = $_GET["model"];
+$length = $_GET["length"];
+if (isset($brand) && isset($model) && isset($length) && $role == "administrator"){
+    include "database.php";
+    $sql = "insert into Skis (brand, model, length) values ('$brand', '$model', '$length');";
+    /** @noinspection PhpUndefinedVariableInspection */
+    $database->query($sql);
+    $database->close();
+}
+include "skis add.html";
