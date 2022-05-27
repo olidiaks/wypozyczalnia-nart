7109d5fead3f3d2183b5d8f1a589a01df35304f0
Oliwier Stępniak <oliwierstepniak@gmail.com>
2022-05-27 22:19:34 +0200

    Fix validation when somebody use auto complete

--- register.php
+++ register.php
@@ -1,34 +1,27 @@
 <?php
-session_start();
-
-$errorEmpty = false;
-$errorEmail = false;
-$errorPassword = false;
-$isEmailEngaged = true;
-$isPasswordEngaged = true;
-
-if (isset($_POST['keypress'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $_SESSION['isValidationGood'] = true;
-
-    include 'database.php';
+/**
+ * @param mixed $email
+ * @param mixed $password
+ * @param mysqli $database
+ * @return bool[]
+ */
+function validateRegisterForm(mixed $email, mixed $password, mysqli $database): array
+{
+    $errorEmpty = false;
+    $errorEmail = false;
+    $errorPassword = false;
+    $isGoodValidation = true;
 
     if (empty($email) && empty($password)) {
         echo "<span class='form-error'>E-mail nie został podany.</span>";
         $errorEmpty = true;
-        $_SESSION['isValidationGood'] = false;
-    }
-    if (empty($password)) {
-        echo "<span class='form-error'>Hasło nie zostało podane.</span>";
-        $errorEmpty = true;
-        $_SESSION['isValidationGood'] = false;
+        $isGoodValidation = false;
     }
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "<span class='form-error'>Podany email nie jest prawidłowy. Przykładowy email wygląda tak lama@lamisiwat.olidiaks</span>";
         $errorEmail = true;
-        $_SESSION['isValidationGood'] = false;
+        $isGoodValidation = false;
     }
 
     $sql = "select * from Users where email = '$email';";
@@ -37,67 +30,92 @@ if (isset($_POST['keypress'])) {
 
     if ($query->num_rows != 0) {
         echo "<span class='form-error'>Podany e-mail został już wykorzystany.</span>";
-        $isEmailEngaged = false;
-        $_SESSION['isValidationGood'] = false;
+        $isGoodValidation = false;
     }
 
     if (strlen($password) < 8) {
         echo "<span class='form-error'>Podane hasło jest za krótkie.</span>";
         $errorPassword = true;
-        $_SESSION['isValidationGood'] = false;
+        $isGoodValidation = false;
     }
 
-    if ($_SESSION['isValidationGood']) {
+    if ($isGoodValidation) {
         echo "<span class='form-success'>Wszystko jest ok.</span>";
     }
+    return array($errorEmpty, $errorEmail, $errorPassword, $isGoodValidation);
+}
+
+/**
+ * @param $errorEmpty
+ * @param $errorEmail
+ * @param $errorPassword
+ * @return void
+ */
+function validateJs($errorEmpty, $errorEmail, $errorPassword): void
+{
+
+    ?>
+    <script>
+        errorEmpty = '<?php echo $errorEmpty;?>';
+        errorEmail = '<?php echo $errorEmail;?>';
+        errorPassword = '<?php echo $errorPassword;?>';
+
+        if (errorEmpty) {
+            form.forEach(element => {
+                if (element.tagName === "input") {
+                    element.classList.add("input-error");
+                }
+            })
+        }
+
+        if (errorEmail) {
+            emailForm.classList.remove("input-success")
+            emailForm.classList.add("input-error");
+        } else {
+            emailForm.classList.remove("input-error");
+            emailForm.classList.add("input-success");
+        }
+
+        if (errorPassword) {
+            passwordForm.classList.remove("input-success")
+            passwordForm.classList.add("input-error");
+        } else {
+            passwordForm.classList.remove("input-error");
+            passwordForm.classList.add("input-success");
+        }
+
+
+    </script>
+    <?php
+}
+
+session_start();
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+include 'database.php';
+/** @noinspection PhpUndefinedVariableInspection */
+list($errorEmpty, $errorEmail, $errorPassword, $isGoodValidation) = validateRegisterForm($email, $password, $database);
+
+if (isset($_POST['keypress'])) {
+
+    validateJs($errorEmpty, $errorEmail, $errorPassword);
+
     $database->close();
-} else if ($_POST['email'] && $_POST['password'] && $_SESSION['isValidationGood']) {
-    include 'database.php';
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+
+} else if (isset($database) && isset($email) && isset($password) && $isGoodValidation) {
     $sql = $_POST['secure-code'] == '0000' ?
         "insert into Users (email, password, role) value ('$email', '$password', 'administrator');" :
         "insert into Users (email, password, role) value ('$email', '$password', 'user')";
     /** @noinspection PhpUndefinedVariableInspection */
     $database->query($sql) or die("Nie udało się wprowadzić danych do bazy danych.<br>" . $database->error);
     $database->close();
-
-    header("Location: login.html");
-} else {
-    header("Location: Register.html");
+    include "login.html";
+    echo "Zarejestrowano";
+} else if (isset($database)) {
+    $database->close();
+    include "Register.html";
+    validateJs($errorEmpty, $errorEmail, $errorPassword);
 }
-
-
 ?>
 
-<script>
-    errorEmpty = '<?php echo $errorEmpty;?>';
-    errorEmail = '<?php echo $errorEmail;?>';
-    errorPassword = '<?php echo $errorPassword;?>';
-
-    if (errorEmpty) {
-        form.forEach(element => {
-            if (element.tagName === "input") {
-                element.classList.add("input-error");
-            }
-        })
-    }
-
-    if (errorEmail) {
-        emailForm.classList.remove("input-success")
-        emailForm.classList.add("input-error");
-    } else {
-        emailForm.classList.remove("input-error");
-        emailForm.classList.add("input-success");
-    }
-
-    if (errorPassword) {
-        passwordForm.classList.remove("input-success")
-        passwordForm.classList.add("input-error");
-    } else {
-        passwordForm.classList.remove("input-error");
-        passwordForm.classList.add("input-success");
-    }
-
-
-</script>
