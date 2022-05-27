390e240b113660602bb24625404a639589abbe7c
olidiaks <oliwierstepniak@gmail.com>
2022-05-27 10:13:27 +0200

    Fix adding to data to database

--- lend backend.php
+++ lend backend.php
@@ -9,7 +9,10 @@ $dataReturn = $_GET["dataReturn"];
 if (isset($idUser) &&isset($skiBoots) && isset($skis) && isset($skiPole) && isset($helmet) && isset($dataReturn)){
     include "database.php";
     $sql = "insert into lend (idUser, idSkis, idSkiBoots, idSkiPole, idHelmet, dateLend, dateReturn) 
-    value($idUser,$skis,$skiBoots,$skiPole,$helmet,'". time() ."', '$dataReturn');";
+    value($idUser,$skis,$skiBoots,$skiPole,$helmet, CURRENT_DATE, '$dataReturn');";
+    /** @noinspection PhpUndefinedVariableInspection */
+    $database->query($sql) or die("Nie udało się dodać danych do bazy danych");
+    $database->close();
     include "lend.php";
     echo "<p>Zamówienie zostało dodane</p>";
 }
