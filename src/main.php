40078f6b3fc4589d7020f1fea4a0bb5e51d5658f
Oliwier Stępniak <oliwierstepniak@gmail.com>
2022-06-09 23:03:22 +0200

    Add hiding adding links when login to uouse.r

--- src/main.php
+++ src/main.php
@@ -8,15 +8,15 @@
 <body>
 
 
-    <div id="header">
-        <a href="whats%20actually%20landed.php?isActually=1">Co aktualnie jest wypożyczone.</a>&nbsp;&nbsp;
-        <a href="whats%20actually%20landed.php">Co był lub jest wypożyczone.</a>&nbsp;&nbsp;
-        <a href="logout.php">Wyloguj się.</a>&nbsp;&nbsp;
-    </div>
-    <div id="include">
-        <?php include("whats actually landed.php") ?>
-    </div>
-
+<div id="header">
+    <a href="whats%20actually%20landed.php?isActually=1">Co aktualnie jest wypożyczone.</a>&nbsp;&nbsp;
+    <a href="whats%20actually%20landed.php">Co był lub jest wypożyczone.</a>&nbsp;&nbsp;
+    <a href="logout.php">Wyloguj się.</a>&nbsp;&nbsp;
+</div>
+<div id="include">
+    <?php include("whats actually landed.php") ?>
+</div>
+<?php if ($_SESSION['role'] == "administrator") { ?>
     <div id="footer">
         <a href="add%20skis.html">Dodaj narty</a>&nbsp;&nbsp;
         <a href="add%20helmets.html">Dodaj kaski</a>&nbsp;&nbsp;
@@ -24,8 +24,7 @@
         <a href="add%20ski%20boots.html">Dodaj buty.</a>&nbsp;&nbsp;
         <a href="lend.php">Wypożycz</a>&nbsp;&nbsp;
     </div>
-
-
+<?php } ?>
 </body>
 <script src="main.js"></script>
 </html>
